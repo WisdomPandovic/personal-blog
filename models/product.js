ddfbb41249be0a11f977bd1453659662722d8dca
Wisdom Onwuchekwa <144015437+WisdomPandovic@users.noreply.github.com>
2025-03-27 19:11:52 +0100

    Update product.js

--- models/product.js
+++ models/product.js
@@ -7,6 +7,7 @@ const ProductSchema = new mongoose.Schema({
     price: { type: String, required: true },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'category',},
     color: [{ type: String, required: false, unique: false }],
+    size: [{ type: String, required: true, unique: false }],
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
     date: { type: Date, default: Date.now },
 });

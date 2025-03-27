b82a7d768b576fbf31addb7191e1e377075e07bf
Wisdom Onwuchekwa <144015437+WisdomPandovic@users.noreply.github.com>
2025-03-27 13:31:08 +0100

    Add files via upload

---
+++ models/product.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    images: [{ type: String, required: true }], 
+    info: { type: String, required: true },
+    price: { type: String, required: true },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'category',},
+    color: [{ type: String, required: false, unique: false }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+    date: { type: Date, default: Date.now },
+});
+
+const Product = mongoose.model('products', ProductSchema);
+
+module.exports = Product;

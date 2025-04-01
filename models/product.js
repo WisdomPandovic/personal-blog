c1f473ea15ac01f7414fc2d0b3572fbf79c3476a
Wisdom Onwuchekwa <144015437+WisdomPandovic@users.noreply.github.com>
2025-04-01 10:46:20 +0100

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,11 +1,33 @@
+// const mongoose = require('mongoose');
+
+// const ProductSchema = new mongoose.Schema({
+//     title: { type: String, required: true },
+//     images: [{ type: String, required: true }], 
+//     info: { type: String, required: true },
+//     price: { type: String, required: true },
+//     category: { type: mongoose.Schema.Types.ObjectId, ref: 'category',},
+//     color: [{ type: String, required: false, unique: false }],
+//     size: [{ type: String, required: true, unique: false }],
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },
+//     date: { type: Date, default: Date.now },
+// });
+
+// const Product = mongoose.model('products', ProductSchema);
+
+// module.exports = Product;
+
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    images: [{ type: String, required: true }], 
+    images: { 
+        type: Map, 
+        of: [String], // A map of color to image array
+        required: true 
+    }, 
     info: { type: String, required: true },
     price: { type: String, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'category',},
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'category' },
     color: [{ type: String, required: false, unique: false }],
     size: [{ type: String, required: true, unique: false }],
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true },

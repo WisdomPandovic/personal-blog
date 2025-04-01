2e86c88b53e836ba48cfab5c529bfad473beb30a
Wisdom Onwuchekwa <144015437+WisdomPandovic@users.noreply.github.com>
2025-04-01 23:50:37 +0100

    Add files via upload

---
+++ utils/emailService.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+// Create a transporter using SMTP transport
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USERNAME, // Use environment variables to protect sensitive information
+        pass: process.env.EMAIL_PASSWORD
+    },
+});
+
+// Function to send email
+const sendEmail = (to, subject, text) => {
+    const mailOptions = {
+        from: process.env.EMAIL_USERNAME,
+        to,
+        subject,
+        text,
+    };
+
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+                reject(error);
+            } else {
+                console.log('Email sent:', info.response);
+                resolve(info.response);
+            }
+        });
+    });
+};
+
+module.exports = sendEmail;

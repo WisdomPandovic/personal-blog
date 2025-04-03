f51e92ecbd844f866db8b44b8240d7826bb63622
Wisdom Onwuchekwa <144015437+WisdomPandovic@users.noreply.github.com>
2025-04-03 22:52:52 +0100

    Update order.js

--- models/order.js
+++ models/order.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  email: { type: String, required: true },
   items: [
     {
       title: String,

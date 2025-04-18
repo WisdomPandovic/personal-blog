b85fda0cdb4755b05ead4ca4f63a3f5e48aeded2
Wisdom Onwuchekwa <144015437+WisdomPandovic@users.noreply.github.com>
2025-04-18 01:39:37 +0100

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const transactionSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "users",
     required: true,
   },
   orderId: {
